582f2492d08b6847b62028cc4e0f3c423725674d
scottfro <skscotkennedy@gmail.com>
2015-05-28 13:17:26 +0100

    Server.js

--- code1/www/js/serve.js
+++ code1/www/js/serve.js
@@ -1,42 +1,86 @@
-var mongo = require('mongodb').MongoClient;
-//var bson = require('bson');
+var restify = require('restify');
+var mongojs = require('mongojs');
 
-//var db = mongoose.connection;
+var db = mongojs('mongodb://AdminScott:6tfc7ygv?@ds62797.mongolab.com:62797/dummy_info', ['dUser'])
 
-var uri = "mongodb://AdminScott:6tfc7ygv?@ds062797.mongolab.com:62797/dummy_info";
+var server = restify.createServer();
 
-mongo.connect(uri, function(err, db)
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.listen(62797, function()
+{
+  console.log('Console started at 62797');
+});
+
+server.get("/dUser", function(req, res, next)
+{
+  db.dUser.find(function(err, dUser)
+  {
+    res.writeHead(200,
+    {
+      'Content-Type': 'application/json; cahrset=utf-8'
+    });
+    res.end(JSON.stringify(dUser));
+  });
+  return next();
+});
+//2nd attempt
+/*var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+
+var server = new Server('ds062797.mongolab.com', 62797, {auto_reconnect : true});
+var db = new Db('dummy_info', server);
+
+db.open(function(err, client)
+{
+  console.log('server running');
+
+  client.authenticate('AdminScott', '6tfc7ygv?', function(err,success)
+  {
+    if(err)
+    {
+      console.log('Can not do it', err);
+    }
+    else {
+      console.log('can do it?');
+    }
+  });
+
+  //find all
+  var findDocuments = function(db, callback)
+  {
+    var collection = db.collection('dUser');
+
+    collection.find({});
+  }
+});*/
+
+//3rd try
+/*var http = require('http');
+var mongojs = require('mongojs');
+
+var uri = 'mongodb://AdminScott:6tfc7ygv?@ds062797.mongolab.com:62797/dummy_info';
+var db = mongojs.connect(uri, ['dUser']);
+
+var server = http.createServer(requestHandler);
+
+function requestHandler(request, response)
+{
+  response.writeHead(200, {'Content-Type': 'text/html'});
+}
+
+db.dUser.find({}), function (err, record)
 {
   if(err)
   {
-    console.log("Error: unable to connect to Database");
+    console.log('There was an err');
+    response.end();
     return;
   }
+}
 
-  //your code here
-});
-/*var schema = mongoose.Schema(
-{
-  categories: String,
-  deal: String,
-  code: String
-});
-
-var Post = mongoose.model('posts', schema);
-module.exports = Post;
-
-Post.find({}).exec(function(err, posts)
-{
-  res.render('posts/index', {posts: posts});
-});
-mongoose.connect('mongodb://localhost/advertdb');
-
-db.on('error', function(msg)
-{
-  console.log('Mongoose connectionerror %s', msg);
-});
-
-db.once('open', function()
-{
-  console.log('Mongoose conection made');
-});*/
+server.listen(8888);*/
